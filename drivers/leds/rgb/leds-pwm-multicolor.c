5caf1dc4e745b69d83f8ce470b3274b12d12cfc5
Daniel Strittmatter <daniel@lockitnetwork.com>
2025-02-17 21:37:35 +0100

    leds-pwm-multicolor: always enable PWM

--- drivers/leds/rgb/leds-pwm-multicolor.c
+++ drivers/leds/rgb/leds-pwm-multicolor.c
@@ -50,7 +50,7 @@ static int led_pwm_mc_set(struct led_classdev *cdev,
 			duty = priv->leds[i].state.period - duty;
 
 		priv->leds[i].state.duty_cycle = duty;
-		priv->leds[i].state.enabled = duty > 0;
+		priv->leds[i].state.enabled = 1;
 		ret = pwm_apply_state(priv->leds[i].pwm,
 				      &priv->leds[i].state);
 		if (ret)

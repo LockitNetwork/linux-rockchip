356c219f7e1dd7d52af57b6402b972355b599d50
Daniel Strittmatter <daniel@lockitnetwork.com>
2025-03-10 17:13:48 +0100

    Support GPIO pull config in Rockchip gpiod driver

--- drivers/gpio/gpio-rockchip.c
+++ drivers/gpio/gpio-rockchip.c
@@ -287,6 +287,10 @@ static int rockchip_gpio_set_config(struct gpio_chip *gc, unsigned int offset,
 	unsigned int debounce = pinconf_to_config_argument(config);
 
 	switch (param) {
+	case PIN_CONFIG_BIAS_DISABLE:
+	case PIN_CONFIG_BIAS_PULL_UP:
+	case PIN_CONFIG_BIAS_PULL_DOWN:
+		return gpiochip_generic_config(gc, offset, config);
 	case PIN_CONFIG_INPUT_DEBOUNCE:
 		rockchip_gpio_set_debounce(gc, offset, debounce);
 		/*
